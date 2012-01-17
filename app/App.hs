25c4f2c8433fc43c35ea01d23769e8d5c1539711
Michael Snoyman <michael@snoyman.com>
2012-01-17 14:43:38 +0200

    Work around missing Lazy.hPutStrLn

--- app/App.hs
+++ app/App.hs
@@ -122,7 +122,7 @@ main = withSocketsDo $ do
       let dump = object [ "config" .= cfg
                         , "environment" .= env
                         , "analysis" .= G.toJSON analysis ]
-      maybeWriteFile json $ \h -> BL.hPutStrLn h . encode $ dump
+      maybeWriteFile json $ \h -> BL.hPut h (BL.append (encode dump) "\n")
       maybeWriteFile dump_events $ \h ->
           TL.hPutStr h . toLazyText . csvEvents $ results
       maybeWriteFile output $ \h -> either (writeReport template h)
